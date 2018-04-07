8d4b48456550275e02fc8772d377212a8b8c3210
Paulo Rodriguez <xlaming@programmer.net>
2018-04-06 21:43:31 -0300

    Everything is new

--- api.php
+++ api.php
@@ -1,129 +1,102 @@
 <?php
-/**
- * *** TRADEXAT APIs ***
- * This class is a way to provide you a few tools that will make your life easier;
- * It is simple, but we'll improving it as well...
- * @copyright MIT License. Copyright (c) 2018 Paulo Rodriguez
- * @author Paulo Rodriguez(xLaming)
- * @link https://github.com/xLaming/xat-fairtrade
- * @version 1.0 (stable)
- */
-
-class API
-{
-	
-	/**
-	 * This function is used to load pages using cURL method.
-	 * @param  string $url
-	 * @return string
-	 */
-	private function loadPage($url)
-	{
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-		curl_setopt($ch, CURLOPT_HEADER, false);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		$result = curl_exec($ch);
-		curl_close($ch);
-		return $result;
-	}
-	
-	/**
-	 * This function is used to search powers in fairtrade's database;
-	 * It works using power name or ID;
-	 * You can set type xml or json
-	 * @param  mixed $power
-	 * @param  string $type
-	 * @return mixed
-	 */
-	public function searchPower($power, $type = 'json')
-	{
-		if (empty($power))
-		{
-			return 'Power name cannot be empty!';
-		}
-		$page = $this->loadPage("http://xatproject.com/fairtrade/api.php?action=search_power&power={$power}&type={$type}");
-		$json = json_decode($page, true);
-		if ($json['status'] == 'fail')
-		{
-			if ($json['message'] == 'power_not_found')
-			{
-				return 'This power does not exists!';
-			}
-			else
-			{
-				return 'Unknown error!';
-			}
-		}
-		return $json['power'];
-	}
-	
-	/**
-	 * This function is used to list all powers by categories;
-	 * Categories available are: unlimited, limited, group, hug, game, epic, allp
-	 * You can set type xml or json
-	 * @param  string $cat
-	 * @param  string $type
-	 * @return mixed
-	 */
-	public function listPowersByCategory($cat, $type = 'json')
-	{
-		if (empty($cat))
-		{
-			return 'Category cannot be empty!';
-		}
-		$page = $this->loadPage("http://xatproject.com/fairtrade/api.php?action=list_powers&category={$cat}&type={$type}");
-		$json = json_decode($page, true);
-		if ($json['status'] == 'fail')
-		{
-			if ($json['message'] == 'invalid_category')
-			{
-				return 'This category does not exists!';
-			}
-			else
-			{
-				return 'Unknown error!';
-			}
-		}
-		return $json['powers'];
-	}
-	
-	/**
-	 * This function will list all powers;
-	 * You can set type xml or json
-	 * @param  string $type
-	 * @return array
-	 */
-	public function listPowers($type = 'json')
-	{
-		$page = $this->loadPage("http://xatproject.com/fairtrade/api.php?action=powers&type={$type}");
-		$json = json_decode($page, true);
-		return $json['powers'];
-	}
+/* used to load pages using cURL */
+function loadPage($url) {
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_HEADER, false);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$result = curl_exec($ch);
+	curl_close($ch);
+	return $result;
 }
 
-$API = new API();
+/* load page, decode it and create a variable to store all powers */
+$page   = loadPage('https://xatproject.com/fairtrade/api.php?action=powers');
+$json   = json_decode($page, true);
+$powers = [];
 
-/**
- * Some examples will be listed above, so you can see how it works:
- *
- * => Looking for a power via name:
- * $API->searchPower('gold');
- *
- * => Looking for a power via id:
- * $API->searchPower(153);
- *
- * => Listing all powers by categories:
- * $API->listPowersByCategory('limited');
- * $API->listPowersByCategory('unlimited');
- * $API->listPowersByCategory('epic');
- * $API->listPowersByCategory('game');
- * $API->listPowersByCategory('group');
- *
- * => Listing all powers:
- * $API->listPowers();
- *
- */
+/* get information of each power category */
+foreach ($json['powers'] as $i => $p)
+{
+	if ($p['is_ep']) { $powers['everypower'][] = $p; }
+	if ($p['is_epic']) { $powers['epic'][] = $p; }
+	if ($p['is_allp']) { $powers['allpowers'][] = $p;	}
+	if ($p['is_group']) { $powers['group'][] = $p; }
+	if ($p['limited'] == 1) { $powers['limited'][] = $p; }
+	if ($p['limited'] != 1) { $powers['unlimited'][] = $p; }
+}
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Fairtrade</title>
+	<style>
+		table { border-collapse: collapse; width: 100%; }
+		th, td { padding: 8px; text-align: left; border-bottom: 1px solid #ddd; }
+		tr:hover {background-color:#f5f5f5;}
+	</style>
+</head>
+<body>
+
+<div align="center"> <h2>Total prices</h2> </div>
+<table>
+  <tr>
+    <th>Type</th>
+    <th>Xats</th>
+    <th>Days</th>
+    <th>Powers</th>
+  </tr>
+  <?php
+  /* list total prices xats, days and amount of powers */
+  foreach ($powers as $t => $v):
+  ?>
+  <tr>
+    <td><?php echo ucfirst($t); ?></td>
+    <td><?php echo array_sum(array_column($v, 'min_xats')) . '-' . array_sum(array_column($v, 'max_xats')); ?></td>
+    <td><?php echo array_sum(array_column($v, 'min_xats')) . '-' . array_sum(array_column($v, 'max_xats')); ?></td>
+    <td><?php echo count($v); ?></td>
+  </tr>
+  <?php endforeach; ?>
+</table>
+
+<div align="center"> <h2>Power list</h2> </div>
+<table>
+	<tr>
+		<th>Name</th>
+		<th>Title</th>
+		<th>Store</th>
+		<th>Xats</th>
+		<th>Days</th>
+		<th>Limited</th>
+		<th>Unlimited</th>
+		<th>Game</th>
+		<th>Group</th>
+		<th>Hug</th>
+		<th>Epic</th>
+		<th>Allpowers</th>
+	</tr>
+	<?php
+	/* list all powers prices and info */
+	foreach ($powers['everypower'] as $i => $p):
+	?>
+	<tr>
+		<th><?php echo ucfirst($p['name']); ?></th>
+		<th><?php echo $p['title']; ?></th>
+		<th><?php echo $p['store']; ?></th>
+		<th><?php echo $p['min_xats'] . '-' . $p['max_xats']; ?></th>
+		<th><?php echo $p['min_days'] . '-' . $p['max_days']; ?></th>
+		<th><?php echo $p['limited'] == 1 ? 'Yes' : 'No'; ?></th>
+		<th><?php echo $p['limited'] == 1 ? 'No' : 'Yes'; ?></th>
+		<th><?php echo $p['is_game'] == 1 ? 'Yes' : 'No'; ?></th>
+		<th><?php echo $p['is_group'] == 1 ? 'Yes' : 'No'; ?></th>
+		<th><?php echo $p['is_hug'] == 1 ? 'Yes' : 'No'; ?></th>
+		<th><?php echo $p['is_epic'] == 1 ? 'Yes' : 'No'; ?></th>
+		<th><?php echo $p['is_allp'] == 1 ? 'Yes' : 'No'; ?></th>
+	</tr>
+	<?php endforeach; ?>
+</table>
+</body>
+</html>
